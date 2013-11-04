3067116faa6fd80b4f9548550b30ce1111c0effa
Vincent Lamanna <v@cakemail.com>
2013-11-04 03:33:14 +0000

    WIP #130

--- dist/js/bootstrap-formhelpers.js
+++ dist/js/bootstrap-formhelpers.js
@@ -15648,11 +15648,38 @@ var BFHTimezonesList = {
   };
 
   function getCursorPosition($element) {
-    return $element.selectionStart;
+    var position = 0,
+        selection;
+
+    if (document.selection) {
+      // IE Support
+      $element.focus();
+      selection = document.selection.createRange();
+      selection.moveStart ('character', -$element.value.length);
+      position = selection.text.length;
+    } else if ($element.selectionStart || $element.selectionStart === 0) {
+      position = $element.selectionStart;
+    }
+
+    return position;
   }
   
   function setCursorPosition($element, position) {
-    $element.setSelectionRange(position, position);
+    var selection;
+    
+    if (document.selection) {
+      // IE Support
+      $element.focus ();
+      selection = document.selection.createRange();
+      selection.moveStart ('character', -$element.value.length);
+      selection.moveStart ('character', position);
+      selection.moveEnd ('character', 0);
+      selection.select ();
+    } else if ($element.selectionStart || $element.selectionStart === 0) {
+      $element.selectionStart = position;
+      $element.selectionEnd = position;
+      $element.focus ();
+    }
   }
 
   /* PHONE PLUGIN DEFINITION
