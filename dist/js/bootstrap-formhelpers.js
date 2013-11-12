24e90346fb729ca0db1486a8442c9604d0d026ef
Vincent Lamanna <v@cakemail.com>
2013-11-12 17:27:38 +0000

    changes.

--- dist/js/bootstrap-formhelpers.js
+++ dist/js/bootstrap-formhelpers.js
@@ -15844,7 +15844,7 @@ var BFHTimezonesList = {
       var options;
       
       options = '';
-      this.$element.find('div.bfh-selectbox-option').each(function() {
+      this.$element.find('.bfh-selectbox-option').each(function() {
         options = options + '<li><a tabindex="-1" href="#" data-option="' + $(this).data('value') + '">' + $(this).html() + '</a></li>';
       });
       
--- js/bootstrap-formhelpers-selectbox.js
+++ js/bootstrap-formhelpers-selectbox.js
@@ -41,7 +41,7 @@
       var options;
       
       options = '';
-      this.$element.find('div.bfh-selectbox-option').each(function() {
+      this.$element.find('.bfh-selectbox-option').each(function() {
         options = options + '<li><a tabindex="-1" href="#" data-option="' + $(this).data('value') + '">' + $(this).html() + '</a></li>';
       });
       
