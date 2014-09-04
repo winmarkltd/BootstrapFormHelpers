117166a553c70629fa4ddde81a4452f95bf07211
root <root@AG_192_168_98_101.(none)>
2014-09-04 16:44:39 +0800

    Add funcationality to specify the amount the value should be incremented/decremented, ex: <input type='text' class='bfh-number' data-min='10' data-max='100' data-step='10'>

--- js/bootstrap-formhelpers-number.js
+++ js/bootstrap-formhelpers-number.js
@@ -166,7 +166,10 @@
       
       value = this.getValue();
       
-      value = value + 1;
+      if(this.options.step == undefined)	
+        value = value + 1;
+      else // if use has specified 'data-step' attribute in <input> tag
+	value = value + this.options.step
       
       this.$element.val(value).change();
     },
@@ -176,7 +179,10 @@
       
       value = this.getValue();
       
-      value = value - 1;
+      if(this.options.step == undefined)	
+      	value = value - 1;
+      else // if use has specified 'data-step' attribute in <input> tag
+	value = value - this.options.step
       
       this.$element.val(value).change();
     },
